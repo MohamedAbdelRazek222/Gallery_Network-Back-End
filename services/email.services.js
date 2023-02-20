6d907bd53fbc430246cd285e9f78ccbbf76907e6
Mohamed Abdelrazek <118555438+MohamedAbdelRazek222@users.noreply.github.com>
2023-02-20 13:27:08 +0200

    Update email.services.js

--- services/email.services.js
+++ services/email.services.js
@@ -28,44 +28,4 @@ console.log(`catch error ${e}`);
 }
 
 
-
-
-
-// const sgMail = require('@sendgrid/mail');
-// function sendEmail(dest,message){
-
-//     try{
-    
-//         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-//         const msg = {
-//           to: dest,
-//           from: 'm07med.abdelrazek@gmail.com', // Use the email address or domain you verified above
-//           subject: 'Sending with Twilio SendGrid is Fun',
-//           text: 'and easy to do anywhere, even with Node.js',
-//           html: message,
-//         };
-
-//         //ES8
-//         (async () => {
-//           try {
-//             await sgMail.send(msg);
-//           } catch (error) {
-//             console.error(error);
-        
-//             if (error.response) {
-//               console.error(error.response.body)
-//             }
-//           }
-//         })();
-    
-//     }catch(e){
-    
-//     // res.status(500).json({message:'email error'})
-//     console.log(`catch error ${e}`);
-    
-//     }
-    
-//     }
-
-
 module.exports=sendEmail
