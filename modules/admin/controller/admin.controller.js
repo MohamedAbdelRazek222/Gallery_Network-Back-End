bc9bc04841291561fd76cab130bd80db2bc8e271
Mohamed Abdelrazek <118555438+MohamedAbdelRazek222@users.noreply.github.com>
2023-02-20 13:33:06 +0200

    Update admin.controller.js

--- modules/admin/controller/admin.controller.js
+++ modules/admin/controller/admin.controller.js
@@ -3,12 +3,7 @@ const sendEmail = require("../../../services/email.services")
 
 const getAllUsers=async(req,res) => {
 
-// const users=await userModel.find({role:{$nin :['Admin',"User"]}}) //دي يعني مبتساويس الاتنين دول
-const users=await userModel.find({role:{$nin :['Admin',"User"]}}) //دي يعني بيساوي الاتنين دول
-// const users=await userModel.find({$or:[{role:'Admin'},{age:{$gt : 30}}]}) //or دي 
-// const users=await userModel.find({$or:[{role:'Admin'},{age:{$gte : 30}}]}) //gte يعني اكبر من او يساوي
-// const users=await userModel.find({$or:[{role:'Admin'},{age:{$lte : 30}}]}) //gte يعني اقل من او يساوي
-
+const users=await userModel.find({role:{$nin :['Admin',"User"]}}) 
 
 res.json({messages:"done",users})
 
@@ -19,7 +14,7 @@ res.json({messages:"done",users})
 
 const changeRole=async(req, res)=>{
 
-const {id}=req.params //دي الي بيشاور علي المتغير
+const {id}=req.params     
 const {role}=req.body
 const user=await userModel.findByIdAndUpdate(id,{role},{new:true})
 sendEmail(user.email,`<p>admin has change u role to  ${user.role}</p>`)
@@ -27,7 +22,7 @@ sendEmail(user.email,`<p>admin has change u role to  ${user.role}</p>`)
 // ------------------------------------------------------------
 const blockuser=async(req, res)=>{
 
-const {id}=req.params //دي الي بيشاور علي المتغير
+const {id}=req.params     
 const user=await userModel.findOne({_id:id})
 if (user.role == req.user.role) {
     res.status(401).json({messages:"sorry u are same role"})
